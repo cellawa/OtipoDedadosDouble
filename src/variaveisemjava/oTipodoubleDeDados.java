7c3cdfb9393e4ac76455946d01c55b8370135f7c
louko <w.riosmagalhaes@gmail.com>
2022-08-06 21:11:54 -0300

    Double

---
+++ src/variaveisemjava/oTipodoubleDeDados.java
@@ -0,0 +1,19 @@
+package variaveisemjava;
+
+public class oTipodoubleDeDados {
+	
+	
+     /*main é um metodo auto executavel em Java*/
+	public static void main(String[] args) {
+		double nota1 = 90;
+		double nota2 = 70;
+		double nota3 = 50;
+		double nota4 = 88;
+		double mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("media das notas =" + mediaFinal);
+		
+		
+
+  }
+}
